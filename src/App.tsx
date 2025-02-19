2a99cfdb46705fb2dd8e54f686212feb2268f66a
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-20 01:20:54 +0900

    add pseudo selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,22 @@ const Title = styled.h1`
   font-size: 4rem;
 `
 
+const Link = styled.a`
+  color: blue;
+  :hover {
+    opacity: 0.6;
+    text-decoration: underline;
+  }
+`
+
 function App() {
   return (
     <>
       <div className="container">
         <Title>Styled Components Playground</Title>
+        <p>
+          <Link href="/">Sample Link</Link>
+        </p>
       </div>
     </>
   )
