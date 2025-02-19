4eac8e3230837ee0a81550c42df3ea38f36c79f4
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-20 01:16:14 +0900

    add basic style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 4rem;
+`
+
 function App() {
   return (
     <>
-      <div className="container">Contents</div>
+      <div className="container">
+        <Title>Styled Components Playground</Title>
+      </div>
     </>
   )
 }
